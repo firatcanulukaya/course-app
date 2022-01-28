a8d3e29c88fecbea04971355ba343c45257993c1
Fıratcan Ulukaya <mua.firat@gmail.com>
2022-01-28 15:34:11 +0200

    tüm fonksiyonları çalışır hale getirildi

--- client/src/components/courses/Courses.jsx
+++ client/src/components/courses/Courses.jsx
@@ -1,15 +1,19 @@
-import bg from "../../assets/img/bg4.svg";
-import CourseList from "./CourseList";
 import {useContext, useEffect, useState} from "react";
 import mainContext from "../../MainContext";
+import CourseList from "./CourseList";
 import CourseAddModal from "./CourseAddModal";
+import CourseDeleteModal from "./CourseDeleteModal";
 import plus from "../../assets/img/plus.svg";
+import bg from "../../assets/img/bg4.svg";
+import axios from "axios";
 import deleteIcon from "../../assets/img/delete.svg";
 
 const Courses = () => {
-    const {coursesData, setCoursesData, serverLink} = useContext(mainContext);
-    const [isOpen, setIsOpen] = useState(false);
-    const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+    const {coursesData, setCoursesData, studentsData, setStudentsData, serverLink, setIsModalDeleteBtnDisabled} = useContext(mainContext);
+    const [isOpen, setIsOpen] = useState({
+        add: false,
+        delete: false
+    });
 
     useEffect(() => {
         const fetchData = async () => {
@@ -20,42 +24,59 @@ const Courses = () => {
         fetchData();
     }, [serverLink, setCoursesData]);
 
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await axios(`${serverLink}/api/student/getAll`);
+            setStudentsData(result.data);
+        };
+        fetchData();
+    }, [serverLink, setStudentsData]);
+
+    const deleteCourse = (id) => {
+        axios.delete(`${serverLink}/api/course/delete/${id}`)
+            .then(() => {
+                const newCoursesData = coursesData.filter(course => course.id !== id);
+                setCoursesData(newCoursesData);
+            })
+            .catch(error => console.log(error));
+    }
+
     return(
         <div className="students-container">
             <img src={bg} alt="background" className="homepage-bg"/>
 
             <CourseAddModal
                 onClose={() => {
-                    setIsOpen(false);
-                }} isActive={isOpen}
+                    setIsOpen({...isOpen, add: false});
+                }} isActive={isOpen.add}
             />
 
-            {/*<StudentDeleteModal*/}
-            {/*    onClose={() => {*/}
-            {/*        setIsDeleteModalOpen(false);*/}
-            {/*        setIsDeleteStudentsModalBtnDisabled(true);*/}
-            {/*    }}*/}
-            {/*    isActive={isDeleteModalOpen}*/}
-            {/*/>*/}
+            <CourseDeleteModal
+                onClose={() => {
+                    setIsOpen({...isOpen, delete: false});
+                    setIsModalDeleteBtnDisabled({...setIsModalDeleteBtnDisabled, course: true});
+                }}
+                isActive={isOpen.delete}
+            />
 
             <button className="add-students tooltip blur" data-tip="Add New Course" onClick={() => {
-                setIsOpen(!isOpen);
+                setIsOpen({...isOpen, add: !isOpen.add});
             }}>
                 <img src={plus} alt="plus" className="plus"/>
             </button>
 
-            {/*{studentsData.length === 0 ? "":*/}
-            {/*    <button className="add-students delete-students tooltip" data-tip="Delete All Students"*/}
-            {/*            onClick={() => {*/}
-            {/*                setIsDeleteModalOpen(!isDeleteModalOpen)*/}
-            {/*                isDeleteModalOpen ? setIsDeleteStudentsModalBtnDisabled(true) : setIsDeleteStudentsModalBtnDisabled(true)*/}
-            {/*            }}>*/}
-            {/*        <img src={deleteIcon} alt="plus" className="plus"/>*/}
-            {/*    </button>*/}
-            {/*}*/}
+            {coursesData.length === 0 ? "" :
+                <button className="add-students delete-students tooltip" data-tip="Delete All Courses"
+                        onClick={() => {
+                            setIsOpen({...isOpen, delete: !isOpen.delete});
+                            isOpen.delete ? setIsModalDeleteBtnDisabled({...setIsModalDeleteBtnDisabled, course: true}) : setIsModalDeleteBtnDisabled({...setIsModalDeleteBtnDisabled, course: true})
+                        }}>
+                    <img src={deleteIcon} alt="plus" className="plus"/>
+                </button>
+            }
 
             {coursesData.length === 0 ? <p className="studentsModalError">Nothing to show about courses.</p> :
-                <CourseList/>}
+                <CourseList deleteCourse={deleteCourse}/>}
         </div>
     )
 }
