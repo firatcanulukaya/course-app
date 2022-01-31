6cde9d3c210124234e084937de5a1bd6c4438727
Fıratcan Ulukaya <mua.firat@gmail.com>
2022-01-31 11:48:44 +0200

    express query parser modülü/özelliği eklendi

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const {json} = require("body-parser");
 const cors = require("cors");
+const { queryParser } = require("express-query-parser");
 const app = express()
 
 const studentRoutes = require("./routes/studentRouter");
@@ -9,6 +10,15 @@ const courseRoutes = require("./routes/courseRouter");
 
 app.use(json())
 app.use(cors())
+
+app.use(
+    queryParser({
+        parseNull: true,
+        parseUndefined: true,
+        parseBoolean: true,
+        parseNumber: true
+    })
+)
 
 app.use('/api/student', studentRoutes);
 app.use('/api/class', classRoutes);
