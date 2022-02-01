5585a6f5719106ce4c15322fb63cf5fff783834d
Fıratcan Ulukaya <mua.firat@gmail.com>
2022-02-01 14:14:12 +0200

    öğrenci kaldirma özelliği eklendi

--- client/src/components/classes/ClassList.jsx
+++ client/src/components/classes/ClassList.jsx
@@ -1,83 +1,115 @@
-import Modal from "../students/StudentEditModal";
+import {useContext, useEffect, useState} from "react";
 import {CourseBadge} from "../../styledComponents";
+import mainContext from "../../MainContext";
+import ClassEditModal from "./ClassEditModal";
 import editIcon from "../../assets/img/edit.svg";
 import deleteIcon from "../../assets/img/delete.svg";
-import {useContext} from "react";
-import mainContext from "../../MainContext";
+import timesIcon from "../../assets/img/times.svg";
+import axios from "axios";
+import DeleteModal from "../DeleteModal";
 
-const CourseList = () => {
-    const {classesData} = useContext(mainContext);
+
+const ClassList = ({deleteClass}) => {
+    const {classesData, setClassesData, serverLink} = useContext(mainContext);
+
+    const [classIds, setClassIds] = useState({
+        index: 0,
+        id: 0
+    });
+
+    const [isOpen, setIsOpen] = useState({
+        add: false,
+        edit: false,
+        delete: false
+    });
+
+    const removeStudent = (studentId, classId) => {
+        axios.patch(`${serverLink}/api/class/removeStudent/${studentId}`)
+            .then(res => {
+                const newClassesData = classesData.map(classData => {
+                    if (classData.id === classId) {
+                        const newStudents = classData.students.filter(student => student.id !== studentId);
+                        return {...classData, students: newStudents};
+                    }
+                    return classData;
+                });
+                setClassesData(newClassesData);
+            })
+            .catch(err => console.log(err));
+    };
 
     return (
         <>
-            {/*<Modal*/}
-            {/*    studentIndex={studentIndex} onClose={() => {*/}
-            {/*    setIsOpen(false);*/}
-            {/*    setIndexID(null);*/}
-            {/*}}*/}
-            {/*    indexID={indexID} isActive={isOpen}/>*/}
+            <ClassEditModal onClose={() => {
+                setIsOpen({...isOpen, edit: false});
+                setClassIds({id: 0, index: 0});
+            }} indexID={classIds.index} classId={classIds.id} isActive={isOpen.edit}/>
+
+            <DeleteModal handleDelete={deleteClass} onClose={() => {
+                setIsOpen({...isOpen, delete: false});
+                setClassIds({id: 0, index: 0});
+            }} id={classIds.id} isActive={isOpen.delete} type={"class"}/>
 
             <div className="table-container">
-
                 <div className="limiter">
                     <div className="container-table100">
                         <div className="wrap-table100">
                             <div className="table">
-
                                 <div className="row header">
                                     <div className="cell">
                                         ID
                                     </div>
                                     <div className="cell">
-                                        Class Info
+                                        Course Info
                                     </div>
                                     <div className="cell">
                                         Students
                                     </div>
-                                    <div className="cell">
-
-                                    </div>
-
+                                    <div className="cell"/>
                                 </div>
 
-
                                 {classesData.map((item, index) => (
-
                                     <div className="row" key={index}>
                                         <div className="cell" data-title="ID">
                                             {item.id}
                                         </div>
-                                        <div className="cell" data-title="Student Info">
-                                            <a href={`class/${item.id}`}> {item.className}
-                                            </a>
-
+                                        <div className="cell" data-title="Course Info">
+                                            <a href={`course/${item.id}`}> {item.className} </a>
                                             <p>Total Student Count: {item.students.length}</p>
                                         </div>
-                                        <div className="cell" data-title="Courses">
-
+                                        <div className="cell" data-title="Students">
                                             <ul>
                                                 <li className="student-table-li">
-                                                    {item.students.length > 0 ? item.students.map((student, index) => (
-                                                            <CourseBadge hex="#60A5FA"
-                                                                         key={index}>{student.studentName}</CourseBadge>
-                                                        ))
-                                                        :
-                                                        <li className="student-table-li">
-                                                            <CourseBadge hex="#000">Class has no students
-                                                                yet</CourseBadge>
-                                                        </li>
+                                                    {
+                                                        item.students.length > 0 ? item.students.map((student, index) => (
+                                                            <CourseBadge key={index} hex="#ABCDEF">
+                                                                <a href={`student/${student.id}`}>{student.studentName.length < 20 ? student.studentName : student.studentName.substr(0, 20) + "..."}</a>
+                                                                <button className="tooltip"
+                                                                        data-tip={`Remove ${student.studentName} student from class`}
+                                                                        onClick={() => removeStudent(student.id, item.id)}>
+                                                                    <img src={timesIcon} style={{mixBlendMode: "luminosity"}} alt="times icon"/></button>
+                                                            </CourseBadge>
+                                                        )) : <CourseBadge hex="#B91C1C">
+                                                            Class has not any students yet
+                                                        </CourseBadge>
                                                     }
                                                 </li>
                                             </ul>
-
                                         </div>
                                         <div className="cell" data-title="Edit">
-                                            <img src={editIcon} alt="Edit icon"/>
-                                            <img src={deleteIcon} alt="Delete icon"/>
+                                            <img src={editIcon} style={{cursor: "pointer"}} alt="Edit icon"
+                                                 onClick={() => {
+                                                     setClassIds({id: item.id, index: index});
+                                                     setIsOpen({...isOpen, edit: !isOpen.edit})
+                                                 }}/>
+                                            <img src={deleteIcon} style={{cursor: "pointer"}} onClick={() => {
+                                                setIsOpen({...isOpen, delete: !isOpen.delete});
+                                                setClassIds({...classIds, id: item.id});
+                                            }}
+                                                 alt="Delete icon"/>
                                         </div>
                                     </div>
                                 ))}
-
                             </div>
                         </div>
                     </div>
@@ -87,4 +119,4 @@ const CourseList = () => {
     )
 }
 
-export default CourseList;
+export default ClassList;
