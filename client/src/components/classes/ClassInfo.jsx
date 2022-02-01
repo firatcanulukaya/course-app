7576d048e4d1ac3baed422e05fa6a5c073859e55
Fıratcan Ulukaya <mua.firat@gmail.com>
2022-02-01 15:05:08 +0200

    tüm fonksiyonları çalışır hale getirildi

--- client/src/components/classes/ClassInfo.jsx
+++ client/src/components/classes/ClassInfo.jsx
@@ -1,9 +1,139 @@
+import {useState, useEffect, useContext} from "react";
+import {useParams} from "react-router-dom";
+import DeleteModal from "../DeleteModal";
+import mainContext from "../../MainContext";
+import axios from "axios";
+import {CourseBadge, InfoButtons, InfoCardTag} from "../../styledComponents";
+import bg from "../../assets/img/bg4.svg";
+import photo from "../../assets/img/photo.svg";
+import arrowLeft from "../../assets/img/arrowLeft.svg"
+import {useNavigate} from "react-router-dom";
+import ClassEditModal from "./ClassEditModal";
+import timesIcon from "../../assets/img/times.svg";
+import {InfoBanner} from "../../styledComponents";
+
 const ClassInfo = () => {
-    return(
-        <div>
+    const {serverLink, classesData, setClassesData, setStudentsData, studentsData} = useContext(mainContext)
+    const navigate = useNavigate();
+    const {id} = useParams();
+
+    const [clas, setClas] = useState({})
+    const [isOpen, setIsOpen] = useState({
+        delete: false,
+        edit: false
+    })
+
+    useEffect(() => {
+        axios.get(`${serverLink}/api/class/getAll`)
+            .then(res => {
+                setClassesData(res.data)
+            })
+            .catch(err => console.log(err))
+    }, [serverLink, classesData])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await axios.get(`${serverLink}/api/student/getAll`);
+            setStudentsData(response.data);
+        };
+        fetchData();
+    }, [serverLink, studentsData]);
+
+    useEffect(() => {
+        const classe = classesData.find(c => c.id === parseInt(id))
+        setClas(classe)
+    }, [classesData, id])
+
+    const deleteCourse = (id) => {
+        axios.delete(`${serverLink}/api/course/delete/${id}`)
+            .then(res => navigate("/courses"))
+            .catch(err => console.log(err))
+    }
+
+    const removeStudent = (classId, studentId) => {
+        axios.patch(`${serverLink}/api/class/removeStudent/${studentId}`)
+            .then(() => {
+                const newClassesData = classesData.map(c => {
+                    if (c.id === classId) {
+                        c.students = c.students.filter(s => s.id !== studentId)
+                    }
+                    return c
+                })
+                setClassesData(newClassesData)
+            })
+            .catch(err => console.log(err))
+    }
+
+    return (
+        <div className="infoCardContainer">
+            <DeleteModal id={id} isActive={isOpen.delete} onClose={() => setIsOpen({...isOpen, delete: !isOpen.delete})}
+                         handleDelete={deleteCourse} type="course"/>
+            <ClassEditModal isActive={isOpen.edit} onClose={() => setIsOpen({...isOpen, edit: !isOpen.edit})}
+                             classId={id}/>
+
+            <img src={bg} alt="background" className="card-bg"/>
+            <div className="infoCard">
+                <InfoBanner bgColor="#ABCDEF"/>
+
+                <div className="infoCardTop">
+                    <div className="infoCardTopLeft">
+                        <div className="infoCardTopPhoto">
+                            <img src={photo} alt="profile photo"/>
+                        </div>
+                        <div className="infoCardUtils">
+                            <p>{clas?.classname?.length > 50 ? clas?.className.substr(0, 50) : clas?.className}</p>
+                            <span>Created at: {new Date(clas?.createdAt).toLocaleDateString("en-US", {
+                                year: "numeric",
+                                month: "long",
+                                day: "numeric",
+                            })}</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="infoCardContent">
+
+                    <div className="infoCardContent-section">
+                        <p>Students (total: {clas?.students?.length}):</p>
+
+                        <ul>
+                            <li className="student-table-li">
+                                {clas?.students?.length > 0 ? clas?.students.map((student, index) => (
+                                    <CourseBadge hex="#ABCDEF" key={index}>
+                                        <a href={`/student/${student.id}`}>{student.studentName.length < 20 ? student.studentName : student.studentName.substr(0, 20) + "..."}</a>
+                                        <button className="tooltip"
+                                                data-tip={`Remove ${student.studentName} student from class`}
+                                                onClick={() => removeStudent(clas?.id, student.id)}>
+                                            <img src={timesIcon} style={{mixBlendMode: "luminosity"}} alt="times icon"/></button>
+                                    </CourseBadge>
+                                )) : <InfoCardTag textColor={"red"}>Course has not any students.</InfoCardTag>}
+                            </li>
+                        </ul>
+                    </div>
+
+                </div>
+
+                <div className="infoCardFooter">
+                    <div className="infoCardFooterSection">
+                        <button onClick={() => navigate("/courses")}>
+                            <img src={arrowLeft}/>
+                            <p>Return Back</p>
+                        </button>
+                    </div>
+
+                    <div className="infoCardFooterButtons">
+                        <InfoButtons bgColor="#F1F1F1" textcolor="#23262F"
+                                     onClick={() => setIsOpen({...isOpen, edit: !isOpen.edit})}>Edit</InfoButtons>
+                        <InfoButtons bgColor="#E53535" textColor="#FCFCFD" isHover={true}
+                                     onClick={() => setIsOpen({...isOpen, delete: !isOpen.delete})}>Delete</InfoButtons>
+                    </div>
+
+                </div>
+
+            </div>
 
         </div>
-    )
+    );
 }
 
 export default ClassInfo;
