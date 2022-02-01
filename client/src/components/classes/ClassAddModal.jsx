570d4b7ec63e90c3966ed05d77e10c9ac7299b93
Fıratcan Ulukaya <mua.firat@gmail.com>
2022-02-01 14:38:11 +0200

    tüm fonksiyonları çalışır hale getirildi

--- client/src/components/classes/ClassAddModal.jsx
+++ client/src/components/classes/ClassAddModal.jsx
@@ -1,9 +1,115 @@
-const CourseAddModal = () => {
-    return(
-        <div>
+import xIcon from "../../assets/img/xIcon.svg";
+import {ModalFooterBtn} from "../../styledComponents";
+import {useContext, useEffect, useState} from "react";
+import mainContext from "../../MainContext";
+import Select from "react-select";
+import axios from "axios";
 
+const ClassAddModal = ({isActive, onClose}) => {
+    const {classesData, setClassesData, studentsData, setStudentsData, serverLink} = useContext(mainContext)
+    const [classesValues, setClassesValues] = useState({
+        className: "",
+        studentIds: []
+    });
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch(`${serverLink}/api/student/getAll`);
+            const data = await response.json();
+            setStudentsData(data);
+        };
+        fetchData();
+    }, [serverLink, setStudentsData]);
+
+    const mapStudents = {
+        options: studentsData.map((item) => {
+            return {
+                value: item.id,
+                label: item.studentName
+            }
+        })
+    }
+
+    const handleSave = (classesValues) => {
+        axios.post(`${serverLink}/api/class/create`, {
+            className: classesValues.className
+        }).then(res => {
+            axios.patch(`${serverLink}/api/class/addStudents`, {
+                classId: res.data.id,
+                studentIds: classesValues.studentIds
+            }).then(() => {
+                axios.get(`${serverLink}/api/class/getAll`).then(res => {
+                    setClassesData(res.data);
+                })
+                onClose();
+            })
+        });
+    }
+
+    return (
+        <div className={"studentModal " + (isActive && "active")} id="studentAddModal">
+            <div className="studentModalheader">
+                <h2>Create New Class</h2>
+                <button className="studentModalclose" onClick={() => {
+                    onClose();
+                }}>
+                    <img src={xIcon} alt="X icon"/>
+                </button>
+            </div>
+
+            <form onSubmit={(e) => {
+                e.preventDefault();
+                handleSave(classesValues)
+            }}>
+                <div className="studentModalContent">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col">
+                                <label className="studentModalLabel">Class Name</label>
+                                <input
+                                    type="text"
+                                    className="studentModalInput"
+                                    required
+                                    placeholder="type..."
+                                    value={classesValues.courseName}
+                                    onChange={(e) => {
+                                        setClassesValues({...classesValues, className: e.target.value})
+                                    }}
+                                />
+                            </div>
+
+                            <div className="col">
+                                <label className="studentModalLabel">Student(s)</label>
+                                <Select options={mapStudents.options} isMulti isSearchable autoFocus
+                                        onChange={(e) => setClassesValues({
+                                            ...classesValues,
+                                            studentIds: e.map((item) => item.value)
+                                        })}
+                                        value={classesValues.studentIds ? classesValues.studentIds.map((item) => {
+                                            return {
+                                                value: item,
+                                                label: studentsData.find((student) => student.id === item).studentName
+                                            }
+                                        }) : []}
+                                />
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div className="studentModalfooter">
+                    <ModalFooterBtn bgColor="#fff" textColor="#374151" isStroke={true} strokeColor="#E5E7EB"
+                                    onClick={() => onClose()} type="button" tabIndex="1">
+                        Cancel
+                    </ModalFooterBtn>
+                    <ModalFooterBtn bgColor="#1E40AF" textColor="#fff" type="submit">
+                        Save Changes
+                    </ModalFooterBtn>
+                </div>
+            </form>
         </div>
     )
 }
 
-export default CourseAddModal;
+export default ClassAddModal;
