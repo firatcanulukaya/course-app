3c0c7d4ecb0f7a72f80b3bf918fea55793fcde7d
Fıratcan Ulukaya <mua.firat@gmail.com>
2022-02-11 14:59:10 +0200

    logoya navigate eklendi

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import xIcon from '../assets/img/xIcon.svg'
+import { useNavigate } from "react-router-dom";
 
 const Sidebar = ({sidebarItems, logo, sidebarCollapse}) => {
+
+    const navigate = useNavigate();
+
     return <div className="sidebar">
         <div className="sidebar-top">
             <div className="sidebar-top-left">
                 <div className="sidebarToggle">
                     <img src={xIcon} alt="x Icon" onClick={() => sidebarCollapse()} style={{cursor: "pointer"}}/>
                 </div>
-                <img src={logo} alt="logo"/>
+                <img  onClick={() => navigate("/")}  style={{cursor: "pointer"}} src={logo} alt="logo"/>
                 <p>Course App</p>
             </div>
         </div>
@@ -16,7 +20,7 @@ const Sidebar = ({sidebarItems, logo, sidebarCollapse}) => {
         <div className="sidebar-items">
             <ul className="sidebar-itemnav">
                 {sidebarItems.map((item, index) => {
-                    return <a href={item.link} key={index}>
+                    return <a onClick={() => navigate(`${item.link}`)}  style={{cursor: "pointer"}} key={index}>
                         <img src={item.icon} alt="sidebar item icon"/>
                         <span>{item.name}</span>
 
